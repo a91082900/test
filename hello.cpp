f934df572f3c85e91d9d6c3877ebeb86d93c7184
Jerry Wu <a91082900@gmail.com>
2020-10-20 14:15:01 +0800

    contest template

--- hello.cpp
+++ hello.cpp
@@ -1,14 +1,9 @@
 # include <iostream>
-# include <vector>
-
-# define N 10
-
-
 using namespace std;
-vector<int> adj[N];
 
 int main(){
-    adj[0].push_back(1);
-    adj[1].push_back(0);
-    cout << "Hello SAD!";
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    return 0;
 }
